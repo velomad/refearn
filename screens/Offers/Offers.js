c11de29cf8420bef590726c96f92033c594b4eaf
A0007 <ajaybendre@credenceanalytics.com>
2021-06-17 19:03:50 +0530

    Fixes in offer

--- screens/Offers/Offers.js
+++ screens/Offers/Offers.js
@@ -1,14 +1,53 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import { COLORS, FONTS, SIZES } from "../../constants/theme";
+import { FocusAwareStatusBar } from "../../components";
+import { Card } from '../../components';
 const Offers = () => {
     return (
         <View>
-            <Text>
-            Offers
-            </Text>
+            <FocusAwareStatusBar
+                barStyle="dark-content"
+                backgroundColor={COLORS.white}
+            />
+            <View style={styles.container}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    showsHorizontalScrollIndicator={false}
+                    style={styles.scrollcontainer}
+                >
+                    <View style={styles.cardContainer}>
+                        <Card backgroundColor={COLORS.secondaryLight}>
+                            <View>
+                                <Text style={styles.mkcenter}>ahahah</Text>
+                                <Text style={styles.mkcenter}>ahahah</Text>
+                            </View>
+                        </Card>
+                    </View>
+                </ScrollView>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+    },
+    scrollcontainer: {
+        flex: 1,
+        height: "100%",
+    },
+    cardContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginHorizontal: SIZES.width / 18,
+        paddingHorizontal: SIZES.width / 7,
+    },
+    mkcenter: {
+        textAlign: "center",
+    }
+});
 
 export default Offers
